91cac8abea88a062bae1e2770b4617d928456252
Ali Fraz <alifrazkhan92@gmail.com>
2024-10-15 17:43:57 +0100

    MernWorkOut

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
 import { BrowserRouter, Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Route path="/" component={Navbar} />
+      </BrowserRouter>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
